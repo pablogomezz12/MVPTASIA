05da7fa0d88479eed9c39eae832e1c54bf19898c
Miguelcp23 <miguelcaurin03@gmail.com>
2025-09-14 21:32:32 +0200

    Modificación v2
    Modificación v2

--- pages/tasacion.py
+++ pages/tasacion.py
@@ -1,8 +1,59 @@
 import streamlit as st
+import pandas as pd
+import joblib
+
 st.set_page_config(page_title="Tasación IA", layout="wide")
 # --------- Página principal (Home) ---------
 # Esta será la primera entrada del menú. Las demás páginas deben ir en la carpeta "pages".
 st.header("Tasación de inmuebles mediante IA")
+
+
+mean_prices = {
+    "Alboraya Centro": 233800.0, "Algirós": 344917.780822, "Barrio de la Luz": 152333.333333,
+    "Benicalap": 267405.30303, "Benimaclet": 357065.625, "Camins al Grau": 355594.771144,
+    "Campanar": 421721.0, "Cardenal Benlloch": 186450.0, "Ciutat Vella": 471826.338843,
+    "DP": 154875.0, "El Pla del Real": 501607.843137, "Extramurs": 440052.671296,
+    "Jesús": 253030.786164, "L'Eixample": 534020.52, "L'Olivereta": 188714.89404,
+    "La Constitución - Canaleta": 215605.555556, "La Saïdia": 281952.537037,
+    "Los Juzgados": 255125.0, "Patraix": 275937.655844, "Poblats Marítims": 299262.189873,
+    "Quatre Carreres": 373587.863454, "Rascanya": 183224.910615,
+    "Zona Ausias March": 289666.666667, "Zona Metro": 164966.666667
+}
+
+property_types = ["Edificio", "Penthouse", "Duplex", "Chalet", "Estudio", "Casa de campo"]
+statuses = ["Bueno", "Reformado", "Desconocido", "Nueva promoción"]
+boolean_opts = ["Verdadero", "Falso", "Desconocido"]
+districts = list(mean_prices.keys()) + ["Desconocido"]
+
+# ------------------ Función de preprocesamiento ------------------
+ONEHOT_CATEGORIES = {
+    'propertyType': ['flat', 'penthouse', 'duplex', 'chalet', 'studio', 'countryHouse'],
+    'status': ['good', 'renew', 'DP', 'newdevelopment'],
+    'district': districts
+}
+
+def preprocesar_inputs(input_dict):
+    df = pd.DataFrame([input_dict])
+    df = df.replace({True: 1, False: 0, 'Verdadero': 1, 'Falso': 0, 'Desconocido': 3})
+
+    # Piso
+    df['is_floor_DP'] = df['floor'].apply(lambda x: 1 if x == 'Desconocido' else 0)
+    df['floor'] = df['floor'].replace({'Bajo': 0, 'Entresuelo': 0.5, 'Subsuelo': -0.5})
+    df['floor'] = pd.to_numeric(df['floor'], errors='coerce').fillna(3.3379)
+
+    # One-hot encoding
+    for col in ['district', 'status', 'propertyType']:
+        dummies = pd.get_dummies(df[col], prefix=col, drop_first=False)
+        df = pd.concat([df, dummies], axis=1)
+        df.drop(columns=col, inplace=True)
+
+    for col, categorias in ONEHOT_CATEGORIES.items():
+        for categoria in categorias:
+            col_name = f"{col}_{categoria}"
+            if col_name not in df.columns:
+                df[col_name] = 0
+
+    return df
 
 
 tab1, tab2, tab3, tab4, tab5 = st.tabs(["Datos básicos", "Ubicación", "Anuncio", "Parking y alquiler", "Otros"])
@@ -14,15 +65,15 @@ with tab1:
         tamaño_m2 = st.number_input("Tamaño en (m^2)", min_value=1, value=1, step=1)
         n_hab = st.number_input("Número de habitaciones", min_value=0, value=0, step=1)
         n_bath = st.number_input("Número de baños", min_value=0, value=0, step=1)
-        ascensor = st.selectbox("Ascensor", options=["Verdadero", "Falso", "Desconocido"])
+        ascensor = st.selectbox("Ascensor", options= boolean_opts)
     with col2_db:
-        propiedad = st.selectbox("Tipo de propiedad", options=["Edificio", "Penthouse", "Chalet", "Duplex", "Estudio", "Casa de campo"])
-        estado_propiedad = st.selectbox("Estado de la propiedad", options=["Bueno", "Reformado", "Desconocido", "Nueva promoción"])
-        exterior = st.selectbox("Exterior", options=["Verdadero", "Falso", "Desconocido"])
+        propiedad = st.selectbox("Tipo de propiedad", options= property_types)
+        estado_propiedad = st.selectbox("Estado de la propiedad", options= statuses)
+        exterior = st.selectbox("Exterior", options= boolean_opts)
 
         # Añadir número de plantas y planta, deshabilitando uno si el otro tiene valor
         num_plantas = st.number_input(
-            "Número de plantas",
+            "Número de planta",
             min_value=0,
             value=0,
             step=1,
@@ -48,34 +99,80 @@ with tab3:
     col1_anuncio, col2_anuncio = st.columns(2)
     with col1_anuncio:
         n_fotos = st.number_input("Número de fotos en anuncio", min_value=0, value=0, step=1)
-        plano = st.selectbox("Tiene plano", options=["Verdadero", "Falso", "Desconocido"])
-        video = st.selectbox("Tiene video", options=["Verdadero", "Falso", "Desconocido"])
+        plano = st.selectbox("Tiene plano", options= boolean_opts)
+        video = st.selectbox("Tiene video", options= boolean_opts)
     with col2_anuncio:
-        tour_3d = st.selectbox("Tiene tour 3D", options=["Verdadero", "Falso", "Desconocido"])
-        vista_360 = st.selectbox("Tiene vista 360º", options=["Verdadero", "Falso", "Desconocido"])
-        staging = st.selectbox("Tiene staging", options=["Verdadero", "Falso", "Desconocido"])
+        tour_3d = st.selectbox("Tiene tour 3D", options= boolean_opts)
+        vista_360 = st.selectbox("Tiene vista 360º", options= boolean_opts)
+        staging = st.selectbox("Tiene staging", options= boolean_opts)
 with tab4:
     col1_parking, col2_parking = st.columns(2)
     with col1_parking:
-        parking = st.selectbox("Tiene parking", options=["Verdadero", "Falso", "Desconocido"])
-        parking_precio = st.selectbox("Parking incluido", options=["Verdadero", "Falso", "Desconocido"])
+        parking = st.selectbox("Tiene parking", options= boolean_opts)
+        parking_precio = st.selectbox("Parking incluido", options= boolean_opts)
         precio_parking = st.number_input("Precio plaza de parking", format="%.2f")
     with col2_parking:
-        piso_turistico = st.selectbox("Piso turístico", options=["Verdadero", "Falso", "Desconocido"])
-        tiene_contrato = st.selectbox("Tiene contrato de alquiler", options=["Verdadero", "Falso", "Desconocido"])
-        ocupa = st.selectbox("Está ocupado", options=["Verdadero", "Falso", "Desconocido"])
+        piso_turistico = st.selectbox("Piso turístico", options= boolean_opts)
+        tiene_contrato = st.selectbox("Tiene contrato de alquiler", options= boolean_opts)
+        ocupa = st.selectbox("Está ocupado", options= boolean_opts)
 with tab5:
     col1_otros, col2_otros = st.columns(2)
     with col1_otros:
-        promocion_nueva = st.selectbox("Nueva promoción", options=["Verdadero", "Falso", "Desconocido"])
-        promocion_termianda = st.selectbox("Promoción terminada", options=["Verdadero", "Falso", "Desconocido"])
-        trastero = st.selectbox("Tiene trastero", options=["Verdadero", "Falso", "Desconocido"])
+        promocion_nueva = st.selectbox("Nueva promoción", options= boolean_opts)
+        promocion_termianda = st.selectbox("Promoción terminada", options= boolean_opts)
+        trastero = st.selectbox("Tiene trastero", options= boolean_opts)
     with col2_otros:
-        piscina = st.selectbox("Tiene piscina", options=["Verdadero", "Falso", "Desconocido"])
-        zonas_coumnes = st.selectbox("Tiene zonas comunes", options=["Verdadero", "Falso", "Desconocido"])
-        accesible = st.selectbox("Es accesible", options=["Verdadero", "Falso", "Desconocido"])
-        amueblado = st.selectbox("Está amueblado", options=["Verdadero", "Falso", "Desconocido"])
+        piscina = st.selectbox("Tiene piscina", options= boolean_opts)
+        zonas_coumnes = st.selectbox("Tiene zonas comunes", options= boolean_opts)
+        accesible = st.selectbox("Es accesible", options= boolean_opts)
+        amueblado = st.selectbox("Está amueblado", options= boolean_opts)
 
 
 if st.button("Predecir precio"):
-    st.success("¡Funcionalidad en desarrollo!")
+    #st.success("¡Funcionalidad en desarrollo!")
+    try:
+        modelo, columnas_entrenamiento = joblib.load("modelo_xgb.joblib")
+    except Exception as e:
+        st.error(f"Error cargando modelo: {e}")
+    else:
+        # Construcción del diccionario con tus variables
+        input_dict = {
+            "size": tamaño_m2,"rooms": n_hab,"bathrooms": n_bath,"latitude": latitude,"longitude": longitude,
+            "parkingSpacePrice": precio_parking,"propertyType": propiedad,"status": estado_propiedad,"district": distrito,
+            "floor": planta_cat if planta_cat else num_plantas,"exterior": exterior,"hasVideo": video,"newDevelopment": promocion_nueva,
+            "hasLift": ascensor,"hasPlan": plano,"has3DTour": tour_3d,"has360": vista_360,"hasStaging": staging,"newDevelopmentFinished": promocion_termianda,
+            "HasParking": parking,"ParkingIncluded": parking_precio,"contrato_alquiler": tiene_contrato,"ocupado": ocupa,"piscina": piscina,
+            "piso_turistico": piso_turistico,"trastero": trastero,"zonas_comunes": zonas_coumnes,"accesible": accesible,
+            "amueblado": amueblado,"numPhotos": n_fotos
+        }
+
+        # Preprocesamiento
+        df_modelo = preprocesar_inputs(input_dict)
+
+        # Asegurarse de que estén todas las columnas esperadas
+        for col in columnas_entrenamiento:
+            if col not in df_modelo.columns:
+                df_modelo[col] = 0
+        df_modelo = df_modelo[columnas_entrenamiento]
+
+        # Predicción
+        pred = modelo.predict(df_modelo)[0]
+
+        # Comparación con el precio medio de la zona
+        mean_price = mean_prices.get(distrito, 0)
+        diff = pred - mean_price
+        diff_sign = "▲" if diff > 0 else "▼"
+        diff_color = "red" if diff > 0 else "green"
+
+        # Mostrar resultados
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st.metric("Precio estimado", f"{pred:,.2f} €")
+        with col2:
+            st.metric(f"Precio medio en {distrito}", f"{mean_price:,.2f} €")
+        with col3:
+            st.metric(
+                label="Diferencia",
+                value=f"{pred-mean_price:,.2f} €",
+                delta=f"{diff:,.2f} €"
+            )
